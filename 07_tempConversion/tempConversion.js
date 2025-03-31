aea2d2183c402d11c656ec04369e828a344c08dd
Spongemeat <195694630+Spongemeat@users.noreply.github.com~>
2025-03-31 18:05:15 -0400

    passed excercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,28 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFarenheit) {
+
+  const a = 32;
+  const b = (5 / 9);
+
+  //celsius = (fahrenheit - 32)(5/9)
+  let celsius = roundToOne((tempFarenheit - a) * (b));
+
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+
+  const c = 32;
+  const d = (9 / 5);
+
+  //fahrenheit = (celcius * (9/5) + 32)
+  let farenheit = roundToOne((tempCelsius * d) + 32);
+
+  return farenheit;
+};
+
+function roundToOne(temp){
+  let rounded = Math.round(temp * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
